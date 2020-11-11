638999a082dd3e81869b2b85e1610e894785b26f
panzerstrike <emesha2030@tutanota.com>
2020-11-11 13:43:29 +0700

    Fix Admin
    dashboard, admin header, header dropdown now working as supposed to be

--- client/src/Components/Utilities/Header.jsx
+++ client/src/Components/Utilities/Header.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import { useQuery } from "react-query";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Dropdown } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 import { GlobalContext } from "../../Context/GlobalContext";
 import Spinner from "./Spinner";
-import { API } from "../../Config/api";
+import { API, urlAsset } from "../../Config/api";
 import topIcon from "../../Assets/Images/topIconImage.svg";
+import { TiDocumentAdd } from "react-icons/ti";
+import { RiLogoutBoxRFill } from "react-icons/ri";
 
 function Header() {
   const [state, dispatch] = useContext(GlobalContext);
@@ -19,7 +21,7 @@ function Header() {
     <Spinner />
   ) : error ? (
     <h1>Your Error : {error.message}</h1>
-  ) : (
+  ) : state.user.role === "user" ? (
     <div id="header">
       <Navbar className="justify-content-between">
         <ul>
@@ -29,6 +31,7 @@ function Header() {
           <li>
             <NavLink to="/my-collection">My Collection</NavLink>
           </li>
+
           <li>
             <NavLink to="/add-literature">Add Literature</NavLink>
           </li>
@@ -50,7 +53,72 @@ function Header() {
         </Link>
       </Navbar>
     </div>
-  );
+  ) : state.user.role === "admin" ? (
+    <div id="admin-header">
+      <Navbar className="justify-content-between">
+        <ul>
+          <li>
+            <NavLink to="/profile">Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/my-collection">My Collection</NavLink>
+          </li>
+          <li>
+            <NavLink to="/home">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin-dashboard">Dashboard</NavLink>
+          </li>
+        </ul>
+        <Dropdown>
+          <Dropdown.Toggle
+            style={{
+              backgroundColor: "transparent",
+              borderColor: "transparent",
+              outline: "none",
+              boxShadow: "none",
+            }}
+          >
+            <div className="image-container">
+              <img
+                className="image"
+                src={`http://localhost:5000/uploads/img/${state.user.picture}`}
+                // src={require(urlAsset.img + state.user.picture)}
+                alt=""
+              />
+            </div>
+          </Dropdown.Toggle>
+          <Dropdown.Menu alignRight>
+            <Dropdown.Item
+              as={Link}
+              to="/admin/add-book"
+              style={{
+                marginBottom: ".5rem",
+                fontSize: 18,
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <TiDocumentAdd /> Add Literature
+            </Dropdown.Item>
+            <Dropdown.Item
+              as={Link}
+              to="/"
+              style={{
+                marginBottom: ".5rem",
+                fontSize: 18,
+                display: "flex",
+                alignItems: "center",
+              }}
+              onClick={() => dispatch({ type: "LOGOUT" })}
+            >
+              <RiLogoutBoxRFill /> Logout
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar>
+    </div>
+  ) : null;
 }
 
 export default Header;
--- client/src/Pages/Admin.jsx
+++ client/src/Pages/Admin.jsx
@@ -3,6 +3,7 @@ import { Table, Container, Button } from "react-bootstrap";
 import { API } from "../Config/api";
 import { useQuery, useMutation } from "react-query";
 import AdminHeader from "../Components/Utilities/AdminHeader";
+import Header from "../Components/Utilities/Header";
 import Spinner from "../Components/Utilities/Spinner";
 
 function Admin() {
@@ -62,89 +63,92 @@ function Admin() {
   ) : error ? (
     <h1>Your error: {error.message}</h1>
   ) : (
-    <div id="admin-page">
-      <Container fluid style={{ backgroundColor: "white" }}>
-        <h3>
-          <strong>Verification</strong>
-        </h3>
-        <div className="admin-bg">
-          <Table borderless hover>
-            <thead>
-              <tr>
-                <th className="text-center">
-                  <strong>No</strong>
-                </th>
-
-                <th className="text-center">
-                  <strong>Author</strong>
-                </th>
-                <th className="text-center">
-                  <strong>ISBN</strong>
-                </th>
-                <th className="text-center">
-                  <strong>Literature</strong>
-                </th>
-                <th className="text-center">
-                  <strong>Status</strong>
-                </th>
-                <th className="text-center">
-                  <strong>Action</strong>
-                </th>
-              </tr>
-            </thead>
-
-            {literatureData.data.data.literature.map((literature, index) => (
-              <tbody striped>
+    <>
+      <Header />
+      <div id="admin-page">
+        <Container style={{ backgroundColor: "white" }}>
+          <h3>
+            <strong>Verification</strong>
+          </h3>
+          <div className="admin-bg">
+            <Table borderless hover>
+              <thead>
                 <tr>
-                  <td>{index + 1}</td>
-                  <td>{literature.author}</td>
-                  <td>{literature.isbn}</td>
-                  <td>{literature.title}</td>
-                  <td
-                    style={{
-                      color:
-                        literature.status === "Approve"
-                          ? "#0ACF83"
-                          : literature.status === "Cancel"
-                          ? "#FF0742"
-                          : "#F7941E",
-                    }}
-                  >
-                    <strong>
-                      {literature.status === "Approve"
-                        ? "Approve"
-                        : literature.status === "Cancel"
-                        ? "Cancel"
-                        : "Waiting"}
-                    </strong>
-                  </td>
-                  <td className="d-flex">
-                    <Button
-                      variant="primary"
-                      size="sm"
-                      onClick={() => approveLiterature(literature.id)}
-                    >
-                      Approve
-                    </Button>
-                    <div
-                      className="buttonActionSeparator"
-                      style={{ marginLeft: "5px" }}
-                    />
-                    <Button
-                      variant="danger"
-                      size="sm"
-                      onClick={() => cancelLiterature(literature.id)}
-                    >
-                      Cancel
-                    </Button>
-                  </td>
+                  <th className="text-center">
+                    <strong>No</strong>
+                  </th>
+
+                  <th className="text-center">
+                    <strong>Author</strong>
+                  </th>
+                  <th className="text-center">
+                    <strong>ISBN</strong>
+                  </th>
+                  <th className="text-center">
+                    <strong>Literature</strong>
+                  </th>
+                  <th className="text-center">
+                    <strong>Status</strong>
+                  </th>
+                  <th className="text-center">
+                    <strong>Action</strong>
+                  </th>
                 </tr>
-              </tbody>
-            ))}
-          </Table>
-        </div>
-      </Container>
-    </div>
+              </thead>
+
+              {literatureData.data.data.literature.map((literature, index) => (
+                <tbody striped>
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{literature.author}</td>
+                    <td>{literature.isbn}</td>
+                    <td>{literature.title}</td>
+                    <td
+                      style={{
+                        color:
+                          literature.status === "Approved"
+                            ? "#0ACF83"
+                            : literature.status === "Cancelled"
+                            ? "#FF0742"
+                            : "#F7941E",
+                      }}
+                    >
+                      <strong>
+                        {literature.status === "Approved"
+                          ? "Approve"
+                          : literature.status === "Cancelled"
+                          ? "Cancelled"
+                          : "Waiting"}
+                      </strong>
+                    </td>
+                    <td className="d-flex">
+                      <Button
+                        variant="primary"
+                        size="sm"
+                        onClick={() => approveLiterature(literature.id)}
+                      >
+                        Approve
+                      </Button>
+                      <div
+                        className="buttonActionSeparator"
+                        style={{ marginLeft: "5px" }}
+                      />
+                      <Button
+                        variant="danger"
+                        size="sm"
+                        onClick={() => cancelLiterature(literature.id)}
+                      >
+                        Cancelled
+                      </Button>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 }
 
